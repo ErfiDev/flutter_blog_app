10ce45f62f4b47c563d29523fe1d526e7278ba1e
erfanhanifezade <erfanhanifezade@gmail.com>
2022-04-08 15:23:04 +0430

    complete blog card design

--- lib/src/blog_tmp.dart
+++ lib/src/blog_tmp.dart
@@ -10,15 +10,31 @@ class BlogTmp extends StatelessWidget {
   @override
   Widget build(ctx) {
     return Container(
-      margin: EdgeInsets.all(8),
+      margin: EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5), //border corner radius
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), //color of shadow
+            spreadRadius: 2, //spread radius
+            blurRadius: 5, // blur radius
+            offset: Offset(0, 2), // changes position of shadow
+            //first paramerter of offset is left-right
+            //second parameter is top to down
+          ),
+          //you can set more BoxShadow() here
+        ],
+      ),
       width: double.infinity,
       height: 400,
-      child: ClipRRect(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
               child: CachedNetworkImage(
                 imageUrl: blog.cover ?? '',
                 placeholder: (context, url) =>
@@ -37,30 +53,33 @@ class BlogTmp extends StatelessWidget {
                 },
               ),
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: MouseRegion(
                       child: Text(
                         blog.title!,
                         style: TextStyle(fontSize: 25),
                       ),
+                      cursor: SystemMouseCursors.click,
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.red,
-                    ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text('From: ${blog.author}',
+                        style: TextStyle(fontSize: 20)),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+        ],
       ),
     );
   }
